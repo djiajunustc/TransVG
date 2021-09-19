a5b991f6452a6fda9bc00e9feb9cb24594fe1b6c
Jiajun Deng <dengjj@mail.ustc.edu.cn>
2021-09-19 09:29:05 +0800

    add script examples for each dataset

--- test.sh
+++ test.sh
@@ -1,5 +1,21 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
-output_dir=./outputs/test_referit
 
-python -m torch.distributed.launch --nproc_per_node=8 --use_env evaluation.py --data_root ./ln_data/ --batch_size 32 --output_dir $output_dir --model_name TransVG --dataset referit --max_query_len 20 --dec_layer 0 --backbone resnet101 --num_workers 4 --imsize 640 --eval_set test --eval_model /data/dengjj/VG/iccv_exp/TransVG/outputs/20210315/referit/r101_transvg/checkpoint.pth 
+# ReferItGame
+python -m torch.distributed.launch --nproc_per_node=8 --use_env eval.py --batch_size 32 --num_workers 4 --bert_enc_num 12 --detr_enc_num 6 --backbone resnet50 --dataset referit --max_query_len 20 --eval_set test --eval_model ../released_models/TransVG_referit.pth --output_dir ./outputs/referit_r50
+
+
+# # RefCOCO
+# python -m torch.distributed.launch --nproc_per_node=8 --use_env eval.py --batch_size 32 --num_workers 4 --bert_enc_num 12 --detr_enc_num 6 --backbone resnet50 --dataset unc --max_query_len 20 --eval_set testA --eval_model ../released_models/TransVG_unc.pth --output_dir ./outputs/refcoco_r50
+
+
+# # RefCOCO+
+# python -m torch.distributed.launch --nproc_per_node=8 --use_env eval.py --batch_size 32 --num_workers 4 --bert_enc_num 12 --detr_enc_num 6 --backbone resnet50 --dataset unc+ --max_query_len 20 --eval_set testA --eval_model ../released_models/TransVG_unc+.pth --output_dir ./outputs/refcoco_plus_r50
+
+
+# # RefCOCOg g-split
+# python -m torch.distributed.launch --nproc_per_node=8 --use_env eval.py --batch_size 32 --num_workers 4 --bert_enc_num 12 --detr_enc_num 6 --backbone resnet50 --dataset gref --max_query_len 40 --eval_set val --eval_model ../released_models/TransVG_gref.pth --output_dir ./outputs/refcocog_gsplit_r50
+
+
+# # RefCOCOg u-split
+# python -m torch.distributed.launch --nproc_per_node=8 --use_env eval.py --batch_size 32 --num_workers 4 --bert_enc_num 12 --detr_enc_num 6 --backbone resnet50 --dataset gref_umd --max_query_len 40 --eval_set test --eval_model ../released_models/TransVG_gref_umd.pth --output_dir ./outputs/refcocog_usplit_r50
--- train.sh
+++ train.sh
@@ -1,4 +1,20 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
+# ReferItGame
+python -m torch.distributed.launch --nproc_per_node=8 --use_env train.py --batch_size 8 --lr_bert 0.00001 --aug_crop --aug_scale --aug_translate --backbone resnet50 --detr_model ./checkpoints/detr-r50-referit.pth --bert_enc_num 12 --detr_enc_num 6 --dataset referit --max_query_len 20 --output_dir outputs/referit_r50
 
-python -m torch.distributed.launch --nproc_per_node=8 --use_env train.py --data_root ./ln_data/ --batch_size 8 --lr 0.0001 --output_dir ./outputs/2021_08_12/referit --lr_visu_tra 0.00001 --lr_visu_cnn 0.00001 --lr_bert 0.00001 --model_name TransVG --dataset referit --max_query_len 40 --dec_layer 0 --backbone resnet50 --detr_model ./checkpoints/detr-r50.pth --aug_crop --aug_scale --aug_translate
+
+# # RefCOCO
+# python -m torch.distributed.launch --nproc_per_node=8 --use_env train.py --batch_size 8 --lr_bert 0.00001 --aug_crop --aug_scale --aug_translate --backbone resnet50 --detr_model ./checkpoints/detr-r50-unc.pth --bert_enc_num 12 --detr_enc_num 6 --dataset unc --max_query_len 20 --output_dir outputs/refcoco_r50 
+
+
+# # RefCOCO+
+# python -m torch.distributed.launch --nproc_per_node=8 --use_env train.py --batch_size 8 --lr_bert 0.00001 --aug_crop --aug_scale --aug_translate --backbone resnet50 --detr_model ./checkpoints/detr-r50-unc.pth --bert_enc_num 12 --detr_enc_num 6 --dataset unc+ --max_query_len 20 --output_dir outputs/refcoco_plus_r50 --epochs 180 --lr_drop 120
+
+
+# # RefCOCOg g-split
+# python -m torch.distributed.launch --nproc_per_node=8 --use_env train.py --batch_size 8 --lr_bert 0.00001 --aug_scale --aug_translate --aug_crop --backbone resnet50 --detr_model ./checkpoints/detr-r50-gref.pth --bert_enc_num 12 --detr_enc_num 6 --dataset gref --max_query_len 40 --output_dir outputs/refcocog_gsplit_r50
+
+
+# # RefCOCOg umd-split
+# python -m torch.distributed.launch --nproc_per_node=8 --use_env train.py --batch_size 8 --lr_bert 0.00001 --aug_scale --aug_translate --aug_crop --backbone resnet50 --detr_model ./checkpoints/detr-r50-gref.pth --bert_enc_num 12 --detr_enc_num 6 --dataset gref_umd --max_query_len 40 --output_dir outputs/refcocog_usplit_r50
