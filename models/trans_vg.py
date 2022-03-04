5acec6ec69575a93cc7756e3b977f69d38bbb5b8
Jiajun Deng <dengjj@mail.ustc.edu.cn>
2022-03-04 01:06:40 -0500

    support different strides

--- models/trans_vg.py
+++ models/trans_vg.py
@@ -11,8 +11,8 @@ class TransVG(nn.Module):
     def __init__(self, args):
         super(TransVG, self).__init__()
         hidden_dim = args.vl_hidden_dim
-        divisor = args.visual_model_stride
-        self.num_visu_token = int((args.imsize / divisor) ** 2)
+        self.stride = args.visual_model_stride
+        self.num_visu_token = int((args.imsize / self.stride) ** 2)
         self.num_text_token = args.max_query_len
 
         self.visual_branch = build_visual_branch(args)
@@ -26,7 +26,7 @@ class TransVG(nn.Module):
         self.text_proj = nn.Linear(self.linguistic_branch.num_channels, hidden_dim)
 
         self.vl_transformer = build_vl_transformer(args)
-        self.bbox_embed = MLP(hidden_dim, hidden_dim, 4, 3)
+        self.bbox_embed = MLP(hidden_dim, 256, 4, 3)
 
 
     def forward(self, img_data, text_data):
@@ -44,6 +44,12 @@ class TransVG(nn.Module):
         # Visual branch
         visu_out = self.visual_branch(img_data)
         visu_src, visu_mask = visu_out.decompose()
+        if self.stride / 16 > 1:
+            tgt_size = int(16 / self.stride * visu_src.shape[-1])
+            visu_src = F.interpolate(visu_src, size=(tgt_size, tgt_size))
+            visu_mask = visu_mask[None].float()
+            visu_mask = F.interpolate(visu_mask, size=(tgt_size, tgt_size)).to(torch.bool)[0]
+
         visu_src = self.visu_proj(visu_src)
         visu_src = visu_src.flatten(2).permute(2, 0, 1) # (N, B, C)
         visu_mask = visu_mask.flatten(1)
